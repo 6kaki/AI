4f24db402490597578866d7100ce6ed0b89bb62f
p6trick <juniyook@korea.ac.kr>
2022-07-16 01:45:55 +0900

    update

---
+++ CV/opencv_c/cmake-build-debug/CMakeFiles/opencv_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_c.dir/main.cpp.o"
+  "opencv_c"
+  "opencv_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opencv_c/opencv_c/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat img = imread("lena.png");
+    
+    if (img.empty()) {
+        printf("Image read failed");
+        exit(-1);
+    }
+    
+    imshow("image", img);
+    waitKey(0);
+    return 0;
+}
